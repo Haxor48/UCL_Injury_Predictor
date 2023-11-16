a92f3e073642cf5f3725810bcdcd374d0f86bdf6
Haxor48 <connort26@outlook.com>
2023-11-16 10:45:56 -0800

    Build table of all ucl_injuries sorted by date

--- connor_code.py
+++ connor_code.py
@@ -3,6 +3,7 @@ import pandas as pd
 from pandas import DataFrame
 import numpy as np
 from pathlib import Path
+import datetime
 
 pb.cache.enable()
 
@@ -15,20 +16,32 @@ tj = pd.read_csv(Path('tj.csv'))
 ucl_prp = pd.read_csv(Path('ucl_prp.csv'))
 ucl_internal = pd.read_csv(Path('ucl_internal.csv'))
 
+def to_date_list(column):
+    return list(map(str, column.tolist()))
+
+def list_in_date(list):
+    new_list = []
+    for item in list:
+        split = item.split('/')
+        if (split != ['nan']):
+            new_list.append(datetime.date(int(split[2]), int(split[0]), int(split[1])))
+        else:
+            new_list.append(np.nan)
+    return new_list
+
 names = tj['Player'].tolist()
-names.append(ucl_prp['Player'].tolist())
-names.append(ucl_internal['Player'].tolist())
-dates = tj['Date'].tolist()
-dates.append(ucl_prp['Date'].tolist())
-dates.append(ucl_internal['Date'].tolist())
+names += ucl_prp['Player'].tolist()
+names += ucl_internal['Player'].tolist()
+dates = to_date_list(tj['Date'])
+dates += to_date_list(ucl_prp['Date'])
+dates += to_date_list(ucl_internal['Date'])
+dates = list_in_date(dates)
 
 data = {'Name': names,
         'Date':dates}
 
 ucl_injuries = DataFrame(data)
 
-#ucl_injuries.drop_duplicates(subset=['Name', 'Date'], keep=False)
-ucl_injuries.dropna()
-ucl_injuries.sort_values(by='Date', ascending=False)
-
-print(ucl_injuries.head())
+ucl_injuries = ucl_injuries.drop_duplicates(subset=['Name', 'Date'], keep=False)
+ucl_injuries = ucl_injuries.dropna()
+ucl_injuries = ucl_injuries.sort_values(by='Date', ascending=False)
